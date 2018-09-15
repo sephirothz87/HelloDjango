261f9161c358509d9ada9b64b90a0705ce25a3c7
SephirothZ87 <sephiroth.zhong@gmail.com>
2018-09-15 21:30:11 +0800

    重复校验判断

--- jceApp/api.py
+++ jceApp/api.py
@@ -9,13 +9,28 @@ def createSubscriber(request):
 	ch.setLevel(logging.DEBUG)
 	logger.addHandler(ch)
 
-	logger.info('foobar')
-	logger.info(request)
-	logger.info(request.POST)
-	logger.info(request.POST['qxwx_login_id'])
+	# logger.info('foobar')
+	# logger.info(request)
+	# logger.info(request.POST)
+	# logger.info(request.POST['qxwx_login_id'])
 	# logger.info(request.GET)
 	# logger.info(request.GET['qxwx_login_id'])
 
-	conn = Model(qxwx_login_id=request.POST['qxwx_login_id'])
-	conn.save()
-	return HttpResponse("createSubscriber")
+	qxwx_login_id = request.POST['qxwx_login_id']
+
+	try:
+		rdp = Model.objects.get(qxwx_login_id=qxwx_login_id)
+	except Model.DoesNotExist:
+		conn = Model(qxwx_login_id=qxwx_login_id)
+		conn.save()
+		return HttpResponse("createSubscriber")
+		
+
+	# if rdp == None:
+	# 	conn = Model(qxwx_login_id=qxwx_login_id)
+	# 	conn.save()
+	# 	return HttpResponse("createSubscriber")
+	# else:
+	logger.info(rdp)
+	return HttpResponse("already exist")
+	# TODO  根据不同情况返回错误码和信息
