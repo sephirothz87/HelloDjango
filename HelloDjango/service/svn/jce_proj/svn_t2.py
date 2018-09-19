bd019a17874c4fba282c811a6cc7dea894baa004
SephirothZ87 <sephiroth.zhong@gmail.com>
2018-09-19 11:20:37 +0800

    修改临时文件目录

--- HelloDjango/service/svn/jce_proj/svn_t2.py
+++ HelloDjango/service/svn/jce_proj/svn_t2.py
@@ -3,13 +3,17 @@ import os
 import locale
 import subprocess
 import sys
+import difflib
+import time, datetime
 
 v1 = '38592'
 # v2 = '37442'
 
 cmdDiff = 'svn diff -r'
+# diffFile = 'diff/tmp1.diff'
 command = cmdDiff+v1
 # command = 'python --version'
+
 
 print(command)
 
@@ -28,13 +32,18 @@ print (sys.getdefaultencoding())
 # print(type(res))
 # print(res.read())
 
+
+timeTag = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+f = open('diff/'+timeTag+'.diff', 'wb')
 count = 0
 line = res.stdout.readline()
 while line:
-	print(count)
+	# print(count)
 	print(line)
+	f.write(line)
 	line = res.stdout.readline()
 	count+=1
+f.close()
 
 print('over')
 
