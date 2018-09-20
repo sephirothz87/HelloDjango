d7a5b2459be97d18a3da87da5dae476ec19d382e
SephirothZ87 <sephiroth.zhong@gmail.com>
2018-09-21 02:29:19 +0800

    windows存在文件占用问题，先关掉django的日志模块

--- jceApp/api.py
+++ jceApp/api.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 from jceApp.models import Model
 import json
-import logging
+# import logging
 
 def createSubscriber(request):
-	logger = logging.getLogger("django")
+	# logger = logging.getLogger("django")
 
-	logger.info('start')
+	# logger.info('start')
 
 	qxwx_login_id = request.POST['qxwx_login_id']
 
@@ -17,11 +17,11 @@ def createSubscriber(request):
 	except Model.DoesNotExist:
 		conn = Model(qxwx_login_id=qxwx_login_id)
 		conn.save()
-		logger.info('订阅成功')
+		# logger.info('订阅成功')
 		res={'code':0,'msg':'订阅成功'}
 		return HttpResponse(json.dumps(res), content_type="application/json")
 		
-	logger.info(rdp)
-	logger.info('已订阅')
+	# logger.info(rdp)
+	# logger.info('已订阅')
 	res = {'code':1,'msg':'该用户已经订阅'}
 	return HttpResponse(json.dumps(res), content_type="application/json")
